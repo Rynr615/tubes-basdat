a46b014a24a29ccf05d5ea659e5698ff66422264
Ryan Yanuar Pradana <ryanyanuarpradana@gmail.com>
2025-05-30 15:53:31 +0700

    Memperbaiki logika pembelian online

--- function/func_pembelianOnline.php
+++ function/func_pembelianOnline.php
@@ -99,36 +99,55 @@ function generateResiCode($conn) {
 function insertPembelianOnline($conn, $idUser , $idPegawai, $tglPembelian, $jenisPembayaran, $kodeProdukArr, $jumlahArr) {
     $totalHarga = 0;
     $subTotals = [];
-    
+
     foreach ($kodeProdukArr as $i => $kodeProduk) {
         $jumlah = (int)$jumlahArr[$i];
-        $hargaResult = mysqli_query($conn, "SELECT Harga FROM produk WHERE KodeProduk = '$kodeProduk'");
-        if (!$hargaResult || mysqli_num_rows($hargaResult) == 0) {
+
+        // Cek stok dulu
+        $produkQuery = mysqli_query($conn, "SELECT Harga, Stok FROM produk WHERE KodeProduk = '$kodeProduk'");
+        if (!$produkQuery || mysqli_num_rows($produkQuery) == 0) {
             return false;
         }
-        $harga = mysqli_fetch_assoc($hargaResult)['Harga'];
+        $produk = mysqli_fetch_assoc($produkQuery);
+        $harga = $produk['Harga'];
+        $stok = $produk['Stok'];
+
+        if ($jumlah > $stok) {
+            echo "<script>alert('Stok untuk produk $kodeProduk tidak mencukupi!'); window.history.back();</script>";
+            exit;
+        }
+
         $subTotal = $jumlah * $harga;
         $totalHarga += $subTotal;
         $subTotals[] = ['kodeProduk' => $kodeProduk, 'jumlah' => $jumlah, 'subTotal' => $subTotal];
     }
 
     $insertPembelian = "INSERT INTO pembelianonline (idUser , tglPembelian, jenisPembayaran, totalHarga)
-                        VALUES ('$idUser ', '$tglPembelian', '$jenisPembayaran', '$totalHarga')";
+                        VALUES ('$idUser', '$tglPembelian', '$jenisPembayaran', '$totalHarga')";
     if (!mysqli_query($conn, $insertPembelian)) {
         return false;
     }
+
     $idPembelian = mysqli_insert_id($conn);
 
     foreach ($subTotals as $item) {
+        $kodeProduk = $item['kodeProduk'];
+        $jumlah = $item['jumlah'];
+        $subTotal = $item['subTotal'];
+
         $query = "INSERT INTO detailpembelianonline (idPembelian, kodeProduk, jumlahProduk, subTotal)
-                    VALUES ('$idPembelian', '{$item['kodeProduk']}', {$item['jumlah']}, {$item['subTotal']})";
+                    VALUES ('$idPembelian', '$kodeProduk', $jumlah, $subTotal)";
         if (!mysqli_query($conn, $query)) {
+            return false;
+        }
+
+        $updateStok = "UPDATE produk SET Stok = Stok - $jumlah WHERE KodeProduk = '$kodeProduk'";
+        if (!mysqli_query($conn, $updateStok)) {
             return false;
         }
     }
 
     $resi = generateResiCode($conn);
-
     $insertDelivery = "INSERT INTO delivery (Resi, idPembelian, idPegawai)
                         VALUES ('$resi', '$idPembelian', '$idPegawai')";
     if (!mysqli_query($conn, $insertDelivery)) {
@@ -137,6 +156,5 @@ function insertPembelianOnline($conn, $idUser , $idPegawai, $tglPembelian, $jeni
 
     return true;
 }
-
 
 ?>
